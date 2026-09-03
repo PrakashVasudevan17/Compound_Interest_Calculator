9ce94db767b29a08481056829e9d60a07d1f91f3
PrakashVasudevan17 <prakashv1712@gmail.com>
2026-09-03 19:47:11 +0530

    Three files add

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+function calculateInterest(){
+const p = parseFloat(document.getElementById("principal").value);
+const r = parseFloat(document.getElementById("rate").value);
+const t = parseFloat(document.getElementById("time").value);
+
+const result = document.getElementById("result");
+
+if(isNaN(p)|| isNaN(r) ||isNaN(t)){
+    result.innerHTML = "Please fill all fields with correct numbers.";
+    return;
+}
+const interest = (p*r*t)/100;
+const totalAmount = p + interest;
+
+result.innerHTML = `Interest : $${interest.toFixed(2)}, TotalAmount : $${totalAmount.toFixed(2)}`
+
+}
+
+function resetForm(){
+    document.getElementById("principal").value ="";
+    document.getElementById("rate").value ="";
+    document.getElementById("time").value ="";
+
+    result.innerHTML = "Enter values and click calculate."
+}
+
+
+
